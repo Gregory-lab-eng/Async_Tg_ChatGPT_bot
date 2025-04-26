f74a880313a994dc8388815ebda8cd6f73e6edd5
Gregory Strakhov <grzegorz.strachow@gmail.com>
2025-04-26 16:28:13 +0200

    chat GPT added, command random added

--- main.py
+++ main.py
@@ -7,10 +7,16 @@ from aiogram.types import Message
 from aiogram.filters import Command
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
+from openai import OpenAI
+
 
 load_dotenv()
 TOKEN = getenv("BOT_TOKEN")
+OPENAI_API_KEY = getenv("OPENAI_API_KEY")
+
 dp = Dispatcher()
+client = OpenAI()
+
 
 
 @dp.message(Command('start'))
@@ -18,11 +24,26 @@ async def command_start_handler(message: Message) -> None:
     await message.answer(f"Gre loves, {html.bold(message.from_user.full_name)}!")
 
 
+@dp.message(Command('random'))
+async def command_random_handler(message: Message) -> None:
+    response = client.responses.create(
+        model="gpt-4o-mini-2024-07-18",
+        input=str(message),
+        instructions="дай любой рандомный факт"
+    )
+    await message.answer(f"{response.output_text}")
+
+
 @dp.message(F.text)
-async def command_start_handler(message: Message) -> None:
-    await message.answer(f"Gre still loves you, {html.bold(message.from_user.full_name)}!")
+async def text_handler(message: Message) -> None:
+    response = client.responses.create(
+        model="gpt-4o-mini-2024-07-18",
+        input=str(message),
+        instructions="Разговаривай как молодой влюбленные поэт"
+    )
+    await message.answer(f"{response.output_text}")
 
-
+#f"Gre still loves you, {html.bold(message.from_user.full_name)}!
 async def main() -> None:
     # Initialize Bot instance with default bot properties which will be passed to all API calls
     bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
